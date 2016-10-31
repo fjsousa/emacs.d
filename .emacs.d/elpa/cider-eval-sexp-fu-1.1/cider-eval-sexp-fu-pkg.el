ef80a93c77b012ed32e22161bb58b716100c2220
Flávio Sousa <flavio.jsousa@gmail.com>
2016-10-31 20:07:21 +0000

    elpa

---
+++ .emacs.d/elpa/cider-eval-sexp-fu-1.1/cider-eval-sexp-fu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cider-eval-sexp-fu" "1.1" "Briefly highlights an evaluated sexps." '((emacs "24") (highlight "0") (eval-sexp-fu "0.4.0")) :keywords '("languages" "clojure" "cider"))
