50b33e4fcf75f64ea0828db46a27c47a472dfdb6
Flávio Sousa <flavio.jsousa@gmail.com>
2018-04-16 11:58:21 +0100

    clean up package list

--- init.el
+++ init.el
@@ -4,16 +4,10 @@
 
 ;; Define package repositories
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 
 ;; Load and activate emacs packages. Do this first so that the
