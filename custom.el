60012987771383f010da6bdcfd6a71604f6b3eb8
Flávio Sousa <flavio.jsousa@gmail.com>
2019-07-06 15:16:03 +0100

    js2 mode warning on trailing commas

--- custom.el
+++ custom.el
@@ -14,14 +14,10 @@
  '(cljr-ignore-analyzer-errors t)
  '(coffee-tab-width 2)
  '(flyspell-popup-correct-delay 0.4)
+ '(js2-strict-missing-semi-warning nil)
  '(package-selected-packages
    (quote
     (smartparens org-make-toc dash dockerfile-mode ac-js2 company-tern langtool flyspell-popup flycheck flymake ido-vertical-mode org-bullets which-key use-package tide typescript-mode find-file-in-project json-navigator robe enh-ruby-mode feature-mode robe-mode enhanced-ruby-mode logview buffer-move yaml-mode command-log-mode restclient stylus-mode lua-mode jade-mode jade company multiple-cursors list-packages-ext direx tagedit smex)))
  '(show-paren-mode t)
  '(show-paren-style (quote parenthesis))
  '(winner-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.)
