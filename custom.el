0a9bfb8e672bc5a73863006e4fba2719c03387cc
Flávio Sousa <flavio.jsousa@gmail.com>
2019-07-12 10:32:40 +0100

    toc

--- custom.el
+++ custom.el
@@ -21,3 +21,9 @@
  '(show-paren-mode t)
  '(show-paren-style (quote parenthesis))
  '(winner-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
