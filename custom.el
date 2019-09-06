74a5229b7c7fd5605a5e549b17bbda9fe594152b
Flávio Sousa <flavio.jsousa@gmail.com>
2019-09-06 19:14:59 +0100

    customisation

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow bold underline bold bold-italic bold])
+ '(blink-cursor-mode nil)
  '(cider-prompt-for-symbol t)
  '(cljr-auto-clean-ns nil)
  '(cljr-auto-eval-ns-form t)
@@ -14,12 +15,16 @@
  '(cljr-ignore-analyzer-errors t)
  '(coffee-tab-width 2)
  '(flyspell-popup-correct-delay 0.4)
+ '(fringe-mode 0 nil (fringe))
  '(js2-strict-missing-semi-warning nil)
  '(package-selected-packages
    (quote
-    (origami smartparens org-make-toc dash dockerfile-mode ac-js2 company-tern langtool flyspell-popup flycheck flymake ido-vertical-mode org-bullets which-key use-package tide typescript-mode find-file-in-project json-navigator robe enh-ruby-mode feature-mode robe-mode enhanced-ruby-mode logview buffer-move yaml-mode command-log-mode restclient stylus-mode lua-mode jade-mode jade company multiple-cursors list-packages-ext direx tagedit smex)))
+    (expand-region sql-indent beacon origami smartparens org-make-toc dash dockerfile-mode ac-js2 company-tern langtool flyspell-popup flycheck flymake ido-vertical-mode org-bullets which-key use-package tide typescript-mode find-file-in-project json-navigator robe enh-ruby-mode feature-mode robe-mode enhanced-ruby-mode logview buffer-move yaml-mode command-log-mode restclient stylus-mode lua-mode jade-mode jade company multiple-cursors list-packages-ext direx tagedit smex)))
+ '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(show-paren-style (quote parenthesis))
+ '(size-indication-mode t)
+ '(window-divider-default-places t)
  '(winner-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
