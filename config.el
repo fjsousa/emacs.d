4bc832cdb19abe277eb69e5d35a6b30c48020b88
Flávio Sousa <flavio.jsousa@gmail.com>
2020-02-14 11:21:50 +0000

    popwin and direx

--- config.el
+++ config.el
@@ -401,3 +401,9 @@
 
 ;;(setq cider-lein-parameters "with-profile debug,dev repl :headless")
 ;;(setq cider-lein-parameters "with-profile +debug repl :headless")
+
+(require 'direx)
+(require 'popwin)
+(push '(direx:direx-mode :position left :width 35 :dedicated t)
+      popwin:special-display-config)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
